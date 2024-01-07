cdebc212d71ecfc412348674616898d0b13a095a
Marcus K <133644816+MarcusProgram@users.noreply.github.com>
2024-01-07 13:35:16 +0300

    Update jump.lua

--- jump.lua
+++ jump.lua
@@ -1,16 +1,5 @@
-print(46)
-
--- я не кодирую свой код и не ставлю ключи потому что я не 3,14дорас, берите код кто хочет и черпайте знаний
-
-
-
-
-
-
-
-
--- game https://www.roblox.com/games/142823291/Murder-Mystery-2
--- loadstring(game:HttpGet("https://raw.githubusercontent.com/MarcusProgram/RobloxScript/main/mm2.lua"))()
+-- game  https://www.roblox.com/games/9834528893/1-Jump-Every-Second
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/MarcusProgram/RobloxScript/main/jump2.lua"))()
 --
 --███╗░░░███╗░█████╗░██████╗░██╗░░░██╗░█████╗░░██████╗
 --████╗░████║██╔══██╗██╔══██╗██║░░░██║██╔══██╗██╔════╝
@@ -19,8 +8,16 @@ print(46)
 --██║░╚═╝░██║██║░░██║██║░░██║╚██████╔╝╚█████╔╝██████╔╝
 --╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░╚═════╝░░╚════╝░╚═════╝░
 
-local stupid = Instance.new('Animation')
-stupid.AnimationId = 'rbxassetid://148840371'
+
+local chatrem = game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest
+chatrem:FireServer('MARCUS', "All")
+
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Character = LocalPlayer.Character
+local HumanoidRootPart = Character.HumanoidRootPart
+local Humanoid = Character.Humanoid
+
 local plrs = game.Players
 
 local playerNames = {}
@@ -37,211 +34,154 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 
 local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
 local Window = OrionLib:MakeWindow({
-    Name = "MM2 by Marcus",
+    Name = "Jump Sim by Marcus",
     HidePremium = false,
     SaveConfig = true,
     ConfigFolder = "Configs"
 })
 
-game:GetService("Players").LocalPlayer.UserId = 1848960
 
 for _, player in ipairs(players) do
     table.insert(playerNames, player.Name)
 end
-table.insert(playerNames, "No Players")
-
 
 _G.autoFarm = false
-_G.autoFarmXP = false
-
-function getMurder()
-    local players = game.Players:GetPlayers()
-    for _, player in ipairs(players) do
-        local knife = player.Character and (player.Character:FindFirstChild("Knife") or player.Backpack:FindFirstChild("Knife"))
-        if knife then
-            return player.Character:FindFirstChild("HumanoidRootPart")
-        end
-    end
-    return nil
-end
-function getSheriff()
-    local players = game.Players:GetPlayers()
-    for _, player in ipairs(players) do
-        local gun = player.Character and (player.Character:FindFirstChild("Gun") or player.Backpack:FindFirstChild("Gun"))
-        if gun then
-            return player.Character:FindFirstChild("HumanoidRootPart")
-        end
-    end
-    return nil
-end
+_G.autoOpen = false
+_G.delete = false
 
 function auto_Farm()
     while _G.autoFarm == true do
-        if game:GetService("Workspace"):FindFirstChild("Christmas") then 
-            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Christmas.CoinContainer.Coin_Server.CFrame
-        end
-    wait(3)
+      game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").WinPaths["Horror Tower"].TouchPart.CFrame
+      wait(2)
     end
 end
 
-function auto_Farm_XP()
-    while _G.autoFarmXP == true do
-    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-99.3635483, 162.194336, 39.1691856, -0.087949343, 8.68945591e-08, -0.996124923, 9.88126914e-10, 1, 8.71453523e-08, 0.996124923, 6.68007871e-09, -0.087949343)
-    wait(1)
-end
-end
-function teleportTo(target)
-    local humanoidRootPart = player.Character.HumanoidRootPart
-    local targetPosition = target.Character.HumanoidRootPart.Position
-    humanoidRootPart.CFrame = CFrame.new(targetPosition)
+function autoFarmEggs(value)
+   while _G.autoOpen == true do
+      local args = {
+         [1] = workspace:WaitForChild("Eggs"):WaitForChild(value),
+         [2] = false
+     }
+     game:GetService("ReplicatedStorage"):WaitForChild("GlobalFunctions"):WaitForChild("PurchasePet"):FireServer(unpack(args))
+     wait(0.5)
+   end
 end
 
+function delete()
+   while _G.delete == true do
+      local pets = {"Spider", "Witch", "Ghost", "Spook"}
+      local GlobalFunctions = game:GetService("ReplicatedStorage"):WaitForChild("GlobalFunctions")
 
-local Main = Window:MakeTab({
-    Name = "Main",
+      for _, pet in ipairs(pets) do
+         GlobalFunctions:WaitForChild("DeletePet"):FireServer(pet)
+      end
+      wait(0.1)
+   end
+end
+
+local Auto = Window:MakeTab({
+    Name = "Auto",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
-Main:AddSection({
-	Name = "Main:"
+Auto:AddSection({
+	Name = "Auto:"
 })
-Main:AddToggle({
-	Name = "Auto Farm Coin",
+Auto:AddToggle({
+	Name = "Auto Farm",
 	Default = false,
 	Callback = function(Value)
 		_G.autoFarm = Value
-        auto_Farm()
+      auto_Farm()
 	end    
 })
-Main:AddToggle({
-	Name = "Auto Farm XP",
+
+Auto:AddDropdown({
+   Name = "Open Eggs",
+   Default = "NO EGG",
+   Options = {"NO EGG","Basic", "Nature","Pirate","Spooky"},
+   Callback = function(value)
+       if value == "NO EGG" then
+           _G.autoOpen = false
+       else
+           _G.autoOpen = true
+           autoFarmEggs(value)
+       end
+   end
+})
+Auto:AddToggle({
+	Name = "Delete all unnecessary pets (last egg)",
 	Default = false,
 	Callback = function(Value)
-		_G.autoFarmXP = Value
-        auto_Farm_XP()
+		_G.delete = Value
+      delete()
 	end    
 })
-Main:AddButton({
-	Name = "Pick Gun",
-	Callback = function()
-        if game:GetService("Workspace"):FindFirstChild("GunDrop") then
-        local pos = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position
-        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").GunDrop.CFrame
-        wait(0.1)
-        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(pos)
-        else
-            game:GetService("StarterGui"):SetCore("SendNotification",{
-                Title = "WARNING",
-                Text = "GUN NOT DROPPED",
-                Icon = "rbxassetid://4483345998"
-            })
-        end 
-  	end
-})
-Main:AddButton({
-	Name = "Kill All (Murder)",
-	Callback = function()
-        if  game:GetService("Players").LocalPlayer.Character:FindFirstChild("Knife") or game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Knife") then
-            for _, otherPlayer in pairs(game.Players:GetPlayers()) do
-                if otherPlayer ~= player then
-                    teleportTo(otherPlayer)
-                end
-            end
-        else
-            game:GetService("StarterGui"):SetCore("SendNotification",{
-                Title = "WARNING",
-                Text = "U ARE NOT MURDER",
-                Icon = "rbxassetid://4483345998"
-            })
-        end
-  	end
-})
-
-Main:AddButton({
-    Name = "Show names",
-    Callback = function()
-        for _, player in ipairs(game:GetService("Players"):GetPlayers()) do
-            if player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
-                local billGui = Instance.new("BillboardGui")
-                billGui.Name = "NameGui"
-                billGui.AlwaysOnTop = true
-                billGui.Size = UDim2.new(0, 100, 0, 40)
-                billGui.ExtentsOffset = Vector3.new(0, 3, 0)
-                billGui.StudsOffset = Vector3.new(0, 2, 0)
-                billGui.Adornee = player.Character.HumanoidRootPart
-                billGui.Parent = player.Character
-                local nameLabel = Instance.new("TextLabel")
-                nameLabel.Name = "NameLabel"
-                nameLabel.BackgroundTransparency = 1
-                nameLabel.Text = player.Name 
-                nameLabel.Size = UDim2.new(1, 0, 1, 0)
-                nameLabel.Font = Enum.Font.SourceSansBold
-                nameLabel.TextColor3 = Color3.new(1, 1, 1)
-                nameLabel.TextScaled = true
-                nameLabel.Parent = billGui
-            end
-        end
-    end
-})
-
-local isUpdating = false
-local chamsColor = BrickColor.new("White")
-
-function UpdateChams()
-    while isUpdating do
-        for _, player in ipairs(game:GetService("Players"):GetPlayers()) do
-            local character = player.Character
-            if character then
-                local hasKnife = (character:FindFirstChild("Knife") or player.Backpack:FindFirstChild("Knife")) and chamsColor ~= BrickColor.new("Really red")
-                local hasGun = (character:FindFirstChild("Gun") or player.Backpack:FindFirstChild("Gun")) and chamsColor ~= BrickColor.new("Bright blue")
-
-                if hasKnife then
-                    chamsColor = BrickColor.new("Really red")
-                elseif hasGun then
-                    chamsColor = BrickColor.new("Bright blue")
-                else
-                    chamsColor = BrickColor.new("White")
-                end
-
-                if character:FindFirstChild("Chams") then
-                    character.Chams.BrickColor = chamsColor
-                else
-                    local chams = Instance.new("BoxHandleAdornment")
-                    chams.Name = "Chams"
-                    chams.Size = character:GetExtentsSize()
-                    chams.AlwaysOnTop = true
-                    chams.ZIndex = 10
-                    chams.Color3 = chamsColor.Color
-                    chams.Transparency = 0.5
-                    chams.Adornee = character
-                    chams.Parent = character
-                end
-            end
-        end
-        wait()
-    end
-end
-Main:AddSection({
-	Name = "turn it on only after the game starts"
-})
-
-Main:AddToggle({
-    Name = "Chams Update",
-    Callback = function(Value)
-        isUpdating = Value
-        if Value then
-            UpdateChams()
-        end
-    end
-})
-
-
-
 local Teleport = Window:MakeTab({
    Name = "Teleport",
    Icon = "rbxassetid://4483345998",
    PremiumOnly = false
 })
+
+local worldCFrameMap = {
+   Rainbow = game:GetService("Workspace").Zones.BaseEarthZone,
+   Candy = game:GetService("Workspace").Zones.BaseCandyZone,
+   Lava = game:GetService("Workspace").Zones.BaseLavaZone,
+   Moon = game:GetService("Workspace").Zones.BaseMoonZone,
+   Beach = game:GetService("Workspace").Zones.BaseBeachZone,
+   Christmas = game:GetService("Workspace").Zones.BaseChristmasZone,
+   Void = game:GetService("Workspace").Zones.BaseVoidZone,
+   Golden = game:GetService("Workspace").Zones.BaseGoldZone,
+   Horror = game:GetService("Workspace").Zones.BaseHorrorZone,
+}
+
+local shopCFrameMap = {
+   GearShop = game:GetService("Workspace").ShopZones.GearShop,
+   TrailShop = game:GetService("Workspace").ShopZones.TrailShop,
+   EffectShop = game:GetService("Workspace").ShopZones.EffectShop
+}
+
+local dropdownOptions = {
+   "Rainbow", "Candy", "Lava", "Moon", "Beach", "Christmas", "Void", "Golden", "Horror"
+}
+
+Teleport:AddDropdown({
+   Name = "Worlds",
+   Default = "Rainbow",
+   Options = dropdownOptions,
+   Callback = function(value)
+       local selectedWorldCFrame = worldCFrameMap[value]
+       if selectedWorldCFrame then
+         game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(selectedWorldCFrame.Position)
+       end
+   end
+})
+Teleport:AddDropdown({
+   Name = "Shops",
+   Default = "NO SHOP",
+   Options = {"GearShop","TrailShop","EffectShop"},
+   Callback = function(value)
+       local selectedWorldCFrame = shopCFrameMap[value]
+       if selectedWorldCFrame then
+         game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(selectedWorldCFrame.Position)
+       end
+   end
+})
+
+
+
+
+Teleport:AddButton({
+	Name = "VIP Tower",
+	Callback = function()
+      game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-215.420578, 6138.28125, 7947.91211, 1, -4.83065392e-08, 0.0002070685, 4.83087028e-08, 1, -1.04280025e-08, -0.0002070685, 1.04380051e-08, 1)
+  	end
+})
+
+Teleport:AddSection({
+	Name = "Teleport:"
+})
+
 
 Teleport:AddDropdown({
     Name = "Players",
@@ -260,41 +200,14 @@ Teleport:AddDropdown({
         print(selectedplrName)
     end    
 })
-Teleport:AddButton({
-	Name = "To Murder",
-	Callback = function()
-      local mur = getMurder()
-      if mur ~= nil then
-        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame=mur.CFrame
-      else
-        game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = "WARNING",
-            Text = "He's gone or it's you",
-            Icon = "rbxassetid://4483345998"
-        })
-      end
-  	end
-})
-Teleport:AddButton({
-	Name = "To Sheriff",
-	Callback = function()
-      local sher = getSheriff()
-      if sher ~= nil then
-      game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame=sher.CFrame
-      else
-        game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = "WARNING",
-            Text = "He's gone or it's you",
-            Icon = "rbxassetid://4483345998"
-        })
-      end
-  	end
-})
+
 local Player = Window:MakeTab({
     Name = "Player",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
+
+
 
 Player:AddButton({
 	Name = "Enable Player List",
@@ -303,144 +216,9 @@ Player:AddButton({
   	end
 })
 
-Player:AddButton({
-	Name = "To be dead",
-	Callback = function()
-        game:GetService("Workspace").marcusov123123.Humanoid.Sit = true
-  	end
-})
-
-Player:AddButton({
-	Name = "Fly",
-	Callback = function()
-        loadstring(game:HttpGet(('https://pastebin.com/raw/TSjdP9mj'),true))()
-  	end
-})
-
-Player:AddButton({
-	Name = "Noclip",
-	Callback = function()
-        local Workspace = game:GetService("Workspace")
-        local CoreGui = game:GetService("CoreGui")
-        local Players = game:GetService("Players")
-        local Noclip = Instance.new("ScreenGui")
-        local BG = Instance.new("Frame")
-        local Title = Instance.new("TextLabel")
-        local Toggle = Instance.new("TextButton")
-        local StatusPF = Instance.new("TextLabel")
-        local Status = Instance.new("TextLabel")
-        local Plr = Players.LocalPlayer
-        local Clipon = false
-
-        Noclip.Name = "Noclip"
-        Noclip.Parent = game.CoreGui
-
-        BG.Name = "BG"
-        BG.Parent = Noclip
-        BG.BackgroundColor3 = Color3.new(0.0980392, 0.0980392, 0.0980392)
-        BG.BorderColor3 = Color3.new(0.0588235, 0.0588235, 0.0588235)
-        BG.BorderSizePixel = 2
-        BG.Position = UDim2.new(0.149479166, 0, 0.82087779, 0)
-        BG.Size = UDim2.new(0, 210, 0, 127)
-        BG.Active = true
-        BG.Draggable = true
-
-        Title.Name = "Title"
-        Title.Parent = BG
-        Title.BackgroundColor3 = Color3.new(0.266667, 0.00392157, 0.627451)
-        Title.BorderColor3 = Color3.new(0.180392, 0, 0.431373)
-        Title.BorderSizePixel = 2
-        Title.Size = UDim2.new(0, 210, 0, 33)
-        Title.Font = Enum.Font.Highway
-        Title.Text = "Noclip"
-        Title.TextColor3 = Color3.new(1, 1, 1)
-        Title.FontSize = Enum.FontSize.Size32
-        Title.TextSize = 30
-        Title.TextStrokeColor3 = Color3.new(0.180392, 0, 0.431373)
-        Title.TextStrokeTransparency = 0
-
-        Toggle.Parent = BG
-        Toggle.BackgroundColor3 = Color3.new(0.266667, 0.00392157, 0.627451)
-        Toggle.BorderColor3 = Color3.new(0.180392, 0, 0.431373)
-        Toggle.BorderSizePixel = 2
-        Toggle.Position = UDim2.new(0.152380958, 0, 0.374192119, 0)
-        Toggle.Size = UDim2.new(0, 146, 0, 36)
-        Toggle.Font = Enum.Font.Highway
-        Toggle.FontSize = Enum.FontSize.Size28
-        Toggle.Text = "Toggle"
-        Toggle.TextColor3 = Color3.new(1, 1, 1)
-        Toggle.TextSize = 25
-        Toggle.TextStrokeColor3 = Color3.new(0.180392, 0, 0.431373)
-        Toggle.TextStrokeTransparency = 0
-
-        StatusPF.Name = "StatusPF"
-        StatusPF.Parent = BG
-        StatusPF.BackgroundColor3 = Color3.new(1, 1, 1)
-        StatusPF.BackgroundTransparency = 1
-        StatusPF.Position = UDim2.new(0.314285725, 0, 0.708661377, 0)
-        StatusPF.Size = UDim2.new(0, 56, 0, 20)
-        StatusPF.Font = Enum.Font.Highway
-        StatusPF.FontSize = Enum.FontSize.Size24
-        StatusPF.Text = "Status:"
-        StatusPF.TextColor3 = Color3.new(1, 1, 1)
-        StatusPF.TextSize = 20
-        StatusPF.TextStrokeColor3 = Color3.new(0.333333, 0.333333, 0.333333)
-        StatusPF.TextStrokeTransparency = 0
-        StatusPF.TextWrapped = true
-
-        Status.Name = "Status"
-        Status.Parent = BG
-        Status.BackgroundColor3 = Color3.new(1, 1, 1)
-        Status.BackgroundTransparency = 1
-        Status.Position = UDim2.new(0.580952346, 0, 0.708661377, 0)
-        Status.Size = UDim2.new(0, 56, 0, 20)
-        Status.Font = Enum.Font.Highway
-        Status.FontSize = Enum.FontSize.Size14
-        Status.Text = "off"
-        Status.TextColor3 = Color3.new(0.666667, 0, 0)
-        Status.TextScaled = true
-        Status.TextSize = 14
-        Status.TextStrokeColor3 = Color3.new(0.180392, 0, 0.431373)
-        Status.TextWrapped = true
-        Status.TextXAlignment = Enum.TextXAlignment.Left
-
-
-        Toggle.MouseButton1Click:connect(function()
-            if Status.Text == "off" then
-                Clipon = true
-                Status.Text = "on"
-                Status.TextColor3 = Color3.new(0,185,0)
-                Stepped = game:GetService("RunService").Stepped:Connect(function()
-                    if not Clipon == false then
-                        for a, b in pairs(Workspace:GetChildren()) do
-                        if b.Name == Plr.Name then
-                        for i, v in pairs(Workspace[Plr.Name]:GetChildren()) do
-                        if v:IsA("BasePart") then
-                        v.CanCollide = false
-                        end end end end
-                    else
-                        Stepped:Disconnect()
-                    end
-                end)
-            elseif Status.Text == "on" then
-                Clipon = false
-                Status.Text = "off"
-                Status.TextColor3 = Color3.new(170,0,0)
-            end
-        end)
-  	end
-})
-
-Player:AddButton({
-	Name = "Inf Jump",
-	Callback = function()
-        loadstring(game:HttpGet("https://pastebin.com/raw/Q0F1njnh"))()
-  	end
-})
 Player:AddSection({
 	Name = "_______________"
 })
-
 Player:AddSlider({
 	Name = "Walk Speed",
 	Min = 16,
@@ -457,7 +235,7 @@ Player:AddSlider({
 Player:AddSlider({
 	Name = "Jump Power",
 	Min = 0,
-	Max = 300,
+	Max = 3000000000,
 	Default = 100,
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 1,
@@ -466,21 +244,6 @@ Player:AddSlider({
 		game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = Value
 	end    
 })
-
-
-Player:AddSlider({
-	Name = "Gravity",
-	Min = 0,
-	Max = 500,
-	Default = 196,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "Gravity",
-	Callback = function(Value)
-		game:GetService("Workspace").Gravity = Value
-	end    
-})
-
 
 Player:AddSection({
 	Name = "_______________"
@@ -551,4 +314,22 @@ MiscTab:AddButton({
   	end    
 })
 
+local Fun = Window:MakeTab({
+   Name = "Fun",
+   Icon = "rbxassetid://4483345998",
+   PremiumOnly = false
+})
+Fun:AddButton({
+	Name = "Party Health Bar",
+	Callback = function()
+      game:GetService("Players").LocalPlayer.Character.Humanoid.MaxHealth = 1000
+      while wait(.01) do 
+         local random = Random.new()
+         game:GetService("Players").LocalPlayer.Character.Humanoid.Health = random:NextNumber(100, 1000)
+      end
+  	end    
+})
+
 OrionLib.Init()
+
+
